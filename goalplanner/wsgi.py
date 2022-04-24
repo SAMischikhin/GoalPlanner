8ffb4cd27520dbf4326eb3d4accfd52e9bf77235
SAMischikhin <michihin@mail.ru>
2022-04-24 17:55:58 +0300

    2nd

--- goalplanner/wsgi.py
+++ goalplanner/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GoalPlanner project.
+WSGI config for goalplanner project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoalPlanner.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goalplanner.settings')
 
 application = get_wsgi_application()
